39b0e6e1df10e035ef0e3f8c0820ccd5b11a13a9
taniachque <taniachque@gmail.com>
2024-03-06 11:35:07 -0400

    Estilos modificados.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
 import Componente2 from './Componente2';
+import Componente3 from './Componente3';
 
 function App() {
   const [text, setText] = useState('');
@@ -16,6 +17,9 @@ function App() {
       </div>
       <div>
         <Componente2 onTextChange={handleTextChange} />
+      </div>
+      <div>
+        <Componente3 texto={text}/>
       </div>
     </div>
   );
---
+++ src/Componente3.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const generateletras = (texto) => {
+  const letras = texto.split('');
+  return letras.map((letra) => (
+    <span key={letra} className="letra">
+      {letra}
+    </span>
+  ));
+};
+
+const Componente3 = ({ texto }) => {
+  const componentesLetras = generateletras(texto);
+
+  return (
+    <div className="letras">
+      {componentesLetras}
+    </div>
+  );
+};
+
+export default Componente3;
