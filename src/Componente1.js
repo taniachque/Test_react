598e0f8ff98eda460e00b2cd2a33cfe0a2edceef
taniachque <taniachque@gmail.com>
2024-03-06 09:37:36 -0400

    Initial commit- Created componentes.

---
+++ src/Componente1.js
@@ -0,0 +1,11 @@
+const Componente1 = () => {
+    return (
+        <body>
+            <div className="title">
+                <h1>TITULO</h1>
+            </div>
+        </body>
+    );
+}
+ 
+export default Componente1;
